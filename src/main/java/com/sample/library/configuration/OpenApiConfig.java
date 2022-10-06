79d1ea8f407abe47b339c772441947c69db3c885
porntipa <porntip.aramrattana@allianz.com>
2022-10-06 11:25:28 +0700

    Add library-service project

---
+++ src/main/java/com/sample/library/configuration/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.sample.library.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Book Application API")
+                        .description("This is a sample Spring Boot RESTful service using springdoc-openapi and OpenAPI 3.")
+                        .termsOfService("terms")
+                        .contact(new Contact().email("porntip.aramrattana@allianz.com")
+                                .name("Developer: Porntip Aramrattana"))
+                        .license(new License().name("GNU"))
+                        .version("2.0")
+                );
+    }
+
+}
